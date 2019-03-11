ca966515f11c3e3cc6ad20c062a9f96a340fc1b4
DESKTOP-B9N4VK6\Surface <arian_saffarizadeh@hotmail.com>
2019-03-11 14:58:46 -0400

    first scene

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+       player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position;
+    }
+}
